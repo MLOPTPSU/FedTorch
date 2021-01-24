4e3e6d709ec99069f0b9bcd6112ab8f7249dc701
mmkamani7 <mohamadmahdi.kamani@gmail.com>
2021-01-24 12:55:22 -0500

    Replacing progressbar with tqdm

--- fedtorch/components/datasets/loader/utils.py
+++ fedtorch/components/datasets/loader/utils.py
@@ -137,21 +137,6 @@ def download_and_extract(file_path, extract_path, url):
         _extract_archive(file_path,extract_path)
     return
 
-# class MyProgressBar():
-#     def __init__(self):
-#         self.pbar = None
-
-#     def __call__(self, block_num, block_size, total_size):
-#         if not self.pbar:
-#             self.pbar=progressbar.ProgressBar(maxval=total_size)
-#             self.pbar.start()
-
-#         downloaded = block_num * block_size
-#         if downloaded < total_size:
-#             self.pbar.update(downloaded)
-#         else:
-#             self.pbar.finish()
-
 
 class TqdmUpTo(tqdm):
     # Provides `update_to(n)` which uses `tqdm.update(delta_n)`.
